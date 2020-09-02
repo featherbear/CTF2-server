9b84bd4ab527f370657bb2dff1ac0b67e9771753
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-03 02:39:12 +1000

    Update /hello route

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,19 @@ export default function (app, opts, done) {
     return CTF2_GAME_URL ? res.redirect(CTF2_GAME_URL) : 'Try harder'
   })
 
-  app.get('/hello', (req, res) => {
-    return {
-      /* Server alive check */
-      hello: 'world!'
+  app.get('/hello', {
+    schema: {
+      description: 'Server health check'
     }
+  }, (req, res) => {
+    const data = {
+      /* Server alive check */
+      hello: 'world!',
+
+      GAME_URL: CTF2_GAME_URL
+    }
+
+    return Object.fromEntries(Object.entries(data).filter(([key, value]) => typeof value !== 'undefined'))
   })
 
   done()
