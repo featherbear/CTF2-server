b0f0c0783c20b7886911c83ada782569a3983c6b
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-03 16:53:27 +1000

    Add reply decorators

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 export default function (app, opts, done) {
   const { CTF2_GAME_URL } = process.env
 
+  app.decorateReply('OK', function (data) {
+    this.send({ result: true, data })
+  })
+  app.decorateReply('FAIL', function (data, code) {
+    if (typeof code !== 'undefined') this.code(code)
+    this.send({ result: false, data })
+  })
+  app.setErrorHandler(function (error, req, res) {
+    res.FAIL(error.message)
+  })
+
   app.register(require('./auth'), { prefix: '/auth' })
   app.register(require('./score'), { prefix: '/score' })
-
 
   app.get('/', { hide: true }, (req, res) => {
     return CTF2_GAME_URL ? res.redirect(CTF2_GAME_URL) : 'Try harder'
