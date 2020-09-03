41d8f1d7d16480cff6dbcb126d9fafd55591d903
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-03 16:54:56 +1000

    Add Database core

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import fastify from 'fastify'
 import fastifyHelmet from 'fastify-helmet'
 
+import { init as initDatabase } from './lib/db'
+
 require('dotenv').config()
 
 const app = fastify()
 app.register(fastifyHelmet)
+
+const { CTF2_DATABASE } = process.env
+
+if (!CTF2_DATABASE) {
+  console.warn('CTF2_DATABASE variable not set! Defaulting to game.db')
+  app.decorate('db', initDatabase('game.db'))
+} else {
+  app.decorate('db', initDatabase(CTF2_DATABASE))
+}
+
 app.register(require('fastify-jwt'), {
   secret: process.env.CTF2_SECRET || require('crypto').randomBytes(30).toString('hex')
 })
