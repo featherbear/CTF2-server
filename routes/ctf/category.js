08b500dcaa21e54a027433522d3ea60f29f61970
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-08 13:13:27 +1000

    Add remaining category functions

--- routes/ctf/category.js
+++ routes/ctf/category.js
@@ -36,5 +36,50 @@ export default function (app, opts, done) {
     }
   )
 
+  app.delete(
+    '/',
+    {
+      preValidation: [app.authenticate, app.authorise],
+      schema: {
+        description: 'Delete a category',
+        body: {
+          id: { type: 'integer', description: 'Category ID' }
+        }
+      }
+    },
+    async (req, res) => {
+      const { id } = req.body
+
+      try {
+        return res.OK(await Category.delete(id))
+      } catch (e) {
+        return res.FAIL(e.message)
+      }
+    }
+  )
+
+  app.put(
+    '/',
+    {
+      preValidation: [app.authenticate, app.authorise],
+      schema: {
+        description: 'Rename a category',
+        body: {
+          id: { type: 'integer', description: 'Category ID' },
+          name: { type: 'string', description: 'Category Name' }
+        }
+      }
+    },
+    async (req, res) => {
+      const { id, name } = req.body
+
+      try {
+        return res.OK(await Category.rename(id, name))
+      } catch (e) {
+        return res.FAIL(e.message)
+      }
+    }
+  )
+
   done()
 }
