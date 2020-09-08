f6ee5337a53635f8ec030dcb9fec751e28b8e669
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-08 12:48:27 +1000

    Change category route

--- routes/ctf/index.js
+++ routes/ctf/index.js
@@ -1,5 +1,5 @@
 export default function (app, opts, done) {
-  app.register(require('./categories'), { prefix: '/categories' })
+  app.register(require('./category'), { prefix: '/category' })
 
   done()
 }
